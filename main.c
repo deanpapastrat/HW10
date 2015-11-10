168950420b591b5aee3bef81529913750687d524
blakktux <blakktux@gmail.com>
2015-11-09 23:00:10 -0500

    Made game using libraries built earlier.

--- main.c
+++ main.c
@@ -8,25 +8,140 @@
 #include "assets/text.h"
 #include <stdio.h>
 
+typedef struct gameplayer {
+    int speed;
+    int distance;
+    int xpos;
+    int ypos;
+} gameplayer;
+
+enum gamestate {
+    STARTMENU,
+    BRIEF,
+    INGAME,
+    GAMEOVER
+};
+
 //text buffer for writing text
-char lbuf[15];
+char lbuf[80];
 
 int main() {
+    //GBA functions
     REG_DISPCNT = MODE_4 | BG2_EN;
     initPalette();
 
-    fillScreen4(0);
-    sprintf(lbuf, "< Its a spooky ghost");
-    drawString(10, 50, lbuf, 76);
+    //loop vars
+    enum gamestate state = STARTMENU;
+    char buttondown = 0;
 
-    int progress = 500;
+    gameplayer player;
+    player.speed = 0;
+    player.distance = 0;
+    player.xpos = 145;
+    player.ypos = 0;
+
+    int explosionNumber = 1;
+
+    //fill screen to start with black for first render
+    fillScreen4(0);
     while (1) {
         waitForVBlank();
-        drawImage4(0, 0, progress, 0, LONGBACKGROUND_WIDTH, LONGBACKGROUND_HEIGHT, longbackground);
-        drawSprite4(0, 87, 0, 0, PLAYER14_WIDTH, PLAYER14_HEIGHT, 0, player14);
+        if (KEY_DOWN_NOW(BUTTON_SELECT)) {
+            state = STARTMENU;
+            player.xpos = 145;
+            player.ypos = 0;
+            player.distance = 0;
+            player.speed = 0;
+        }
+        switch (state) {
+            case STARTMENU:
+                drawImage4(0, 0, TITLESCREEN_WIDTH, TITLESCREEN_HEIGHT, titlescreen);
+                if (KEY_DOWN_NOW(BUTTON_START)) {
+                    buttondown = 1;
+                } else {
+                    if (buttondown) {
+                        state = BRIEF;
+                        buttondown = 0;
+                    }
+                }
+                break;
+            case BRIEF:
+                drawImage4(0, 0, ROAD_WIDTH, ROAD_HEIGHT, road);
+                drawRect4(8, 25, 120, 18, 0);
+                sprintf(lbuf, "You're in a fast car!");
+                drawString(30, 10, lbuf, 0xfe);
+                drawRect4(8, 55, 218, 30, 0);
+                sprintf(lbuf, "Go as fast as possible without");
+                drawString(60, 10, lbuf, 0xfe);
+                sprintf(lbuf, "crashing. Koenigseggs are expensive");
+                drawString(70, 10, lbuf, 0xfe);
+
+                drawRect4(8, 95, 120, 18, 0);
+                sprintf(lbuf, "Press Start to drive!");
+                drawString(100, 10, lbuf, 0xfe);
+
+                if (KEY_DOWN_NOW(BUTTON_START)) {
+                    buttondown = 1;
+                } else {
+                    if (buttondown) {
+                        state = INGAME;
+                        buttondown = 0;
+                    }
+                }
+                break;
+            case INGAME:
+                if (player.xpos < 60 || player.xpos > 170) {
+                    switch (explosionNumber) {
+                        case 1:
+                            drawSprite4(player.xpos - 120, 200, 0, 0, EXPLOSION1_WIDTH, EXPLOSION1_HEIGHT, 0xE4,
+                                        explosion1);
+                            break;
+                        case 10:
+                            drawSprite4(player.xpos - 120, 200, 0, 0, EXPLOSION2_WIDTH, EXPLOSION2_HEIGHT, 0xE4,
+                                        explosion2);
+                            break;
+                        case 20:
+                            drawSprite4(player.xpos - 120, 200, 0, 0, EXPLOSION3_WIDTH, EXPLOSION3_HEIGHT, 0xE4,
+                                        explosion3);
+                            break;
+                        case 30:
+                            drawSprite4(player.xpos - 120, 200, 0, 0, EXPLOSION4_WIDTH, EXPLOSION4_HEIGHT, 0xE4,
+                                        explosion4);
+                            break;
+                        case 40:
+                            state = GAMEOVER;
+                            break;
+                    }
+                    explosionNumber++;
+                } else {
+                    if (KEY_DOWN_NOW(BUTTON_RIGHT) && player.speed > 0) {
+                        player.xpos++;
+                    } else if (KEY_DOWN_NOW(BUTTON_LEFT) && player.speed > 0) {
+                        player.xpos--;
+                    }
+                    if (KEY_DOWN_NOW(BUTTON_UP) && player.speed < 80) {
+                        player.speed += 1;
+                    } else if (KEY_DOWN_NOW(BUTTON_DOWN) && player.speed > 0) {
+                        player.speed -= 3;
+                        if (player.speed < 0) {
+                            player.speed = 0;
+                        }
+                    }
+                    player.distance -= player.speed / 2;
+                    drawImageOffset4(0, 0, 0, (player.distance % 160) + 160, ROAD_WIDTH, ROAD_HEIGHT, road);
+                    drawSprite4(player.xpos, 100, 0, 0, CAR_WIDTH, CAR_HEIGHT, 0xe4, car);
+                    drawRect4(0, 0, 100, 25, 0);
+                    sprintf(lbuf, "Distance %d feet", -player.distance / 10);
+                    drawString(4, 2, lbuf, 0xfe);
+                    sprintf(lbuf, "Speed: %d mph", player.speed * 3);
+                    drawString(12, 2, lbuf, 0xfe);
+                }
+                break;
+            case GAMEOVER:
+                drawImage4(0, 0, GAMEOVER_WIDTH, GAMEOVER_HEIGHT, gameover);
+                break;
+        }
         vid_flip();
-        while (!KEY_DOWN_NOW(BUTTON_RIGHT));
-        progress++;
     }
 }
 
--- mylib.c
+++ mylib.c
@@ -19,7 +19,10 @@ void waitForVBlank() {
     while (SCANLINECOUNTER < 160);
 }
 
-void drawImage4(int x, int y, int sourcex, int sourcey, int width, int height, const unsigned short *image) {
+void drawImage4(int r, int c, int width, int height, const u16* image) {
+    drawImageOffset4(c, r, 0, 0, width, height, image);
+}
+void drawImageOffset4(int x, int y, int sourcex, int sourcey, int width, int height, const unsigned short *image) {
 
     if (sourcex + 240 > width || sourcey + 160 > height)
         return;
@@ -30,10 +33,10 @@ void drawImage4(int x, int y, int sourcex, int sourcey, int width, int height, c
     height = height;
     xcopies = xcopies / 2;
 
-    //if would go to next row, change copies to hit the 240th pixel and no futher
-//    if (x + xcopies > 240) {
-//        xcopies = (240 - x) / 2;
-//    }
+//    if would go to next row, change copies to hit the 240th pixel and no futher
+    if (x + xcopies > 240) {
+        xcopies = (240 - x) / 2;
+    }
     //if we would start writing to random memory (bad), stop at the 160th row
     if (y + height > 160) {
         maxy = (160 - y);
@@ -41,7 +44,7 @@ void drawImage4(int x, int y, int sourcex, int sourcey, int width, int height, c
 
     for (int iy = 0; iy < maxy; iy++) {
         //source is the image address with the offset of the source looking
-        DMA[3].src = (image + ((sourcey + iy) * width + sourcex)/ 2);
+        DMA[3].src = (image + ((sourcey + iy) * width + sourcex) / 2);
         //dst is the location on the current page, offset by x and y
         DMA[3].dst = &vid_page[((y + iy) * 120 + x)];
         //turn dma on and go for the correct number of transfers to not go over either memory width
@@ -68,9 +71,12 @@ void drawSprite4(int x, int y, int sourcex, int sourcey, int width, int height, 
 
     for (int ix = 0; ix < maxx; ix++) {
         for (int iy = 0; iy < maxy; iy++) {
-            if (*(image + ((sourcey + iy) * width) + sourcex + ix) != colorkey) {
-                drawPixel4(x + ix, y + iy, *(image + ((sourcey + iy) * width) + sourcex + ix));
-            }
+            //the double pixel location
+            u16 *dst = &vid_page[((y + iy) * 240 + (x + ix)) / 2];  // Division by 2 due to u8/u16 pointer mismatch!
+            if ((*(image + ((sourcey + iy) * width) + sourcex + ix) & 0xFF) != colorkey)
+                *dst = *(image + ((sourcey + iy) * width) + sourcex + ix) << 8;    // odd pixel
+            if ((*(image + ((sourcey + iy) * width) + sourcex + ix) & 0xFF) != colorkey)
+                *dst = *dst | *(image + ((sourcey + iy) * width) + sourcex + ix);        // even pixel
         }
     }
 
@@ -88,9 +94,19 @@ void fillScreen4(COLOR clr) {
     drawRect4(0, 0, 240, 160, clr);
 }
 
-void drawRect4(int left, int top, int right, int bottom, COLOR clr) {
-    int ix, iy;
-    for (iy = top; iy < bottom; iy++)
-        for (ix = left; ix < right; ix++)
-            drawPixel4(ix, iy, clr);
+void drawRect4(int x, int y, int width, int height, u8 colorkey) {
+    for (int iy = 0; iy < height; iy++) {
+        //source is the color
+        DMA[3].src = &colorkey;
+        //dst is the location on the current page, offset by x and y
+        DMA[3].dst = &vid_page[((y + iy) * 120 + (x / 2))];
+        //turn dma on and go for the correct number of transfers to not go over either memory width
+        DMA[3].cnt = DMA_ON | DMA_SOURCE_FIXED | (width / 2);
+    }
+
+//    for (int x = r; x <= r + height; x++) {
+//        for (int y = c; y <= c + width; y++) {
+//            drawPixel4(r, c, colorkey);
+//        }
+//    }
 }
